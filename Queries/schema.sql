f8a63dcea41dab3227af3ca9de4cf600d451bc54
mollydorman23 <103781847+mollydorman23@users.noreply.github.com>
2022-05-30 11:43:20 -0500

    updated query

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -49,3 +49,43 @@ CREATE TABLE titles (
      PRIMARY KEY (emp_no)
 );
 SELECT * FROM departments;
+SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM dept_manager;
+DROP TABLE dep_emp CASCADE;
+DROP TABLE titles CASCADE;
+CREATE TABLE dep_emp (
+     emp_no INT NOT NULL,
+     dept_no VARCHAR(4) NOT NULL,
+     from_date DATE NOT NULL,
+     to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+     PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE titles (
+     emp_no INT NOT NULL,
+     title VARCHAR NOT NULL,
+     from_date DATE NOT NULL,
+     to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+     PRIMARY KEY (emp_no)
+);
+DROP TABLE titles CASCADE;
+CREATE TABLE titles (
+     emp_no INT NOT NULL,
+     title VARCHAR NOT NULL,
+     from_date DATE NOT NULL,
+     to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+DROP TABLE titles CASCADE;
+CREATE TABLE titles (
+     emp_no INT NOT NULL,
+     title VARCHAR NOT NULL,
+     from_date DATE NOT NULL,
+     to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+     PRIMARY KEY (emp_no, from_date)
+);
